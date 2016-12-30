c304abf22f6f6ef8fe45db29553c33894bc8e77d
menghong chhay <chhaymenghong@yahoo.com>
2016-12-30 15:24:38 -0800

    add env config

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,7 @@
 // The list of which env maps to which file can be found in `angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  baseUrl: 'https://www.googleapis.com/youtube/v3/search',
+  apiToken:'AIzaSyDOfT_BO81aEZScosfTYMruJobmpjqNeEk'
 };
