0fe42938e54e21b306dab2a822dfa09427c49c25
menghong chhay <chhaymenghong@yahoo.com>
2016-12-30 13:58:45 -0800

    add search result component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { SearchResultComponent } from './search-result/search-result.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchResultComponent
   ],
   imports: [
     BrowserModule,
