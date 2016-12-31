1c7017241fe6583ea96490cb049cbb6fd01e84b6
menghong chhay <chhaymenghong@yahoo.com>
2016-12-30 19:18:36 -0800

    add some styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,13 +10,15 @@ import { SearchResultListComponent } from './search-result-list/search-result-li
 // service
 import { SearchService } from './search.service';
 import { SearchBoxComponent } from './search-box/search-box.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SearchResultComponent,
     SearchResultListComponent,
-    SearchBoxComponent
+    SearchBoxComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
